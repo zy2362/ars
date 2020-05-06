490d019061d15571e922b328781261f7a174ae24
Yang Zihan <yang.zihan@columabi.edu>
2020-05-06 12:26:34 -0400

    views

--- linkall/models.py
+++ linkall/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
-class thing(models.Model):
+class Thing(models.Model):
     name = models.CharField(max_length=100)
     nick_name = models.CharField(max_length=100)
     class_id = models.IntegerField(default=0) # Liquid, solid, etc. To decide icon
@@ -9,16 +9,21 @@ class thing(models.Model):
     autobuy_enable = models.BooleanField(default=True)
     autobuy_url = models.CharField(max_length=500)
     device_mac = models.CharField(max_length=100)
+    owned_by = models.IntegerField(default=0)
     power = models.IntegerField(default=80)
     status = models.CharField(max_length=200)
+    def __str__(self):
+        return "%s(%s:%s)" % (self.name, self.thing_id, self.owned_by)
 
-class user(models.Model):
+class User(models.Model):
     id = models.IntegerField(default=0,primary_key=True)
     nick_name = models.CharField(max_length=100)
     login_name = models.CharField(max_length=100)
     login_password = models.CharField(max_length=200)
+    def __str__(self):
+        return "%s (%s:%s)" % (self.nick_name, self.id, self.login_name)
 
-class paymentMethod(models.Model):
+class PaymentMethod(models.Model):
     user_id = models.IntegerField(default=0)
     card_number = models.CharField(max_length=32)
     card_valid = models.CharField(max_length=16)
--- linkall/views.py
+++ linkall/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import User
 
-# Create your views here.
+def hello(request, name):
+    users = User.objects.all()
+    output = [u.nick_name for u in users]
+    message = "There are %s users in this website. They are: %s" % (len(users), output)
+    return HttpResponse("Hello! Welcome to my home, %s.<br>%s" % (name, message))
+
+def index(request):
+    users = User.objects.all()
+    context = {'users': users}
+    return render(request, 'linkall/index.html', context)
