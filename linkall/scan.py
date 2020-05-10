5f178286d1beed4c63eb9b7c6cecfbc5308223ac
Yang Zihan <yang.zihan@columabi.edu>
2020-05-10 16:51:05 -0400

    fastPush

--- linkall/scan.py
+++ linkall/scan.py
@@ -20,9 +20,8 @@ def scan():
         #print(runout_time)
         #print(betas)
         remains_time = runout_time - int(time())
-        alert(phone, alarm_type="runout", data=remains_time) # for test
-        #if remains_time < 30: # should be 3600 * 24 * 3 in final product
-        #    alert(phone, alarm_type="runout", data=remains_time)
+        if remains_time < 30: # should be 3600 * 24 * 3 in final product
+            alert(phone, alarm_type="runout", data=remains_time)
 
 def parse(datas):
     weights = []
