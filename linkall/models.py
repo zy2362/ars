8cd771314584b89e596c481a1c5178917bad69a4
Yang Zihan <yang.zihan@columabi.edu>
2020-05-10 11:43:02 -0400

    fastPush

--- linkall/models.py
+++ linkall/models.py
@@ -6,5 +6,6 @@ class Thing(models.Model):
     net_weight = models.IntegerField(default=0)
     watch_dog = models.IntegerField(default=0)
     phone = models.CharField(max_length=100, default="0")
+    clear_time = models.IntegerField(default=0)
     def __str__(self):
         return "%s: %sg" % (self.name, self.weight)
