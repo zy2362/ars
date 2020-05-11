0bcc39703dd2b1c6378c4be0aac6ad4f8c5fe5ce
Yang Zihan <yang.zihan@columabi.edu>
2020-05-11 17:08:04 -0400

    fastPush

--- linkall/views.py
+++ linkall/views.py
@@ -13,7 +13,7 @@ from boto3.dynamodb.conditions import Key,Attr
 #==================================
 
 def scan():
-    alpha = 0.01
+    alpha = 0.005
     iteration = 1000
     dynamodb = boto3.resource('dynamodb')
     table = dynamodb.Table('linkall')
